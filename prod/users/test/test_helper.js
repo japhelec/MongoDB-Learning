ce151a84ea2ab044768692f7d49b1e9b55df7d51
Kuei <material3679@gmail.com>
2020-05-06 15:14:54 +0800

    apply mocha to test whether mongoose instance save successgully

--- prod/users/test/test_helper.js
+++ prod/users/test/test_helper.js
@@ -1,14 +1,28 @@
 const mongoose = require("mongoose");
 
-//連接到地方上的 database，並開起新的計劃 database 叫作 user_test
-mongoose.connect("mongodb://localhost/users_test");
-//檢查mongoose 連線
-mongoose.connection
-  // event emitter
-  .once("open", () => {
-    console.log("Good to go!");
-  })
-  // event emitter
-  .on("error", (error) => {
-    console.warn("Warning", error);
+//告知 mocha 在執行測試前先跑這一段
+before((done) => {
+  //連接到地方上的 database，並開起新的計劃 database 叫作 user_test
+  mongoose.connect("mongodb://localhost/users_test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
+  //檢查mongoose 連線
+  //mongoose.connection 用來取得 mogoose.connect 的物件
+  //加上 .once  .on 兩個 event emitter 檢查連線
+  mongoose.connection
+    .once("open", () => {
+      done();
+    })
+    .on("error", (error) => {
+      console.warn("Warning", error);
+    });
+});
+
+//Provided by Mocha, tell Mocha execute below code before any testing
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    //Ready to run the test
+    done();
+  });
+});
